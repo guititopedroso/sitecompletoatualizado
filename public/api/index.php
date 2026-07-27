b567c7f6bee6000ee9f289c13cacf1fb3bf7410a
guitito <guititopedroso@gmail.com>
2026-07-27 14:38:21 +0100

    Restore missing method variable in public/api/index.php

--- public/api/index.php
+++ public/api/index.php
@@ -204,6 +204,7 @@ function saveUpload($fileKey) {
 // --- Routing ---
 initTables();
 
+$method  = $_SERVER['REQUEST_METHOD'];
 $rawPath = !empty($_SERVER['PATH_INFO']) ? $_SERVER['PATH_INFO'] : (!empty($_SERVER['REDIRECT_URL']) ? $_SERVER['REDIRECT_URL'] : parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 // Remove qualquer prefixo /api/ ou /public/api/
 $path    = preg_replace('#^/(public/)?api/(index\.php/)?#', '', $rawPath);
