ad82c8d7bdd89f0e6da9f2510fe520faa3e14e94
guitito <guititopedroso@gmail.com>
2026-07-28 21:56:00 +0100

    Adicionado o video DJI FPV JetSki no Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,7 +24,6 @@ const Hero = () => {
           preload="auto"
           className="w-full h-[120%] object-cover"
         >
-          <source src="https://res.cloudinary.com/dw86u43e6/video/upload/v1/Timeline_1_m0bngd" type="video/mp4" />
           <source src="/hero-video.mp4" type="video/mp4" />
         </video>
       </motion.div>
